ac7b3a58b1bd0d0bf4a269af46c62d66d87f843c
Kerry Wang <kwzeyunwang@gmail.com>
2022-03-26 00:04:53 -0400

    update combobox styling

--- src/ComboWrapper.jsx
+++ src/ComboWrapper.jsx
@@ -19,7 +19,7 @@ export default function ComboWrapper({
         );
   return (
     <Combobox value={value} onChange={onChange}>
-      <div className="relative mt-1">
+      <div className="relative mt-4 md:w-96">
         <div className="relative w-full text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-teal-300 focus-visible:ring-offset-2 sm:text-sm overflow-hidden">
           <Combobox.Input
             className="w-full border-none focus:ring-0 py-2 pl-3 pr-10 text-sm leading-5 text-gray-900"
@@ -50,7 +50,7 @@ export default function ComboWrapper({
                   key={person}
                   className={({ active }) =>
                     `cursor-default select-none relative py-2 pl-10 pr-4 ${
-                      active ? "text-white bg-teal-600" : "text-gray-900"
+                      active ? "text-white bg-gray-400" : "text-gray-900"
                     }`
                   }
                   value={person}
@@ -64,15 +64,18 @@ export default function ComboWrapper({
                       >
                         {person}
                       </span>
-                      {selected ? (
-                        <span
-                          className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
-                            active ? "text-white" : "text-teal-600"
-                          }`}
-                        >
-                          <CheckIcon className="w-5 h-5" aria-hidden="true" />
-                        </span>
-                      ) : null}
+                      {
+                        // Checkmark style
+                        selected ? (
+                          <span
+                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                              active ? "text-white" : "text-emerald-800"
+                            }`}
+                          >
+                            <CheckIcon className="w-5 h-5" aria-hidden="true" />
+                          </span>
+                        ) : null
+                      }
                     </>
                   )}
                 </Combobox.Option>
