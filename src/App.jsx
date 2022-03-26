1bded29c3318c616ec4b759b870d6fd6c86e0a54
Kerry Wang <kwzeyunwang@gmail.com>
2022-03-25 23:49:30 -0400

    read hall and room from json

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,24 @@
 import { useState } from "react";
 import ComboWrapper from "./ComboWrapper";
+import data from "./rooms.json";
 
-const people = [
-  "Durward Reynolds",
-  "Kenton Towne",
-  "Therese Wunsch",
-  "Benedict Kessler",
-  "Katelyn Rohan",
-];
-
-const otherPeople = ["Stephen", "Kerry", "Lena"];
+const halls = Object.keys(data).sort();
 
 export default function App() {
-  const [selectedPerson, setPerson] = useState(people[0]);
-  const [selectedOtherPerson, setOtherPerson] = useState(otherPeople[0]);
+  const [selectedHall, setHall] = useState(halls[0]);
+  const rooms = Object.keys(data[selectedHall]).sort();
+  const [selectedRoom, setRoom] = useState(rooms[0]);
+  if (!rooms.includes(selectedRoom)) {
+    setRoom(rooms[0]);
+  }
+  console.log("ping");
   return (
     <div className="flex flex-col justify-center align-center items-center min-h-screen">
-      <ComboWrapper list={people} value={selectedPerson} onChange={setPerson} />
-      <ComboWrapper
-        list={otherPeople}
-        value={selectedOtherPerson}
-        onChange={setOtherPerson}
-      />
+      <ComboWrapper list={halls} value={selectedHall} onChange={setHall} />
+      <ComboWrapper list={rooms} value={selectedRoom} onChange={setRoom} />
+      <p className="text-white">
+        {selectedHall} {selectedRoom}
+      </p>
     </div>
   );
 }
